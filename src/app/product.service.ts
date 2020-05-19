61ca3f784bf9a01a89e5405667f306c275fbdc7b
shiowwj <shiowweijun@gmail.com>
2020-05-19 14:54:00 +0800

    - part 4

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Http, Response} from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+
+import { Album } from './album';
+
 
 
 @Injectable()
@@ -11,9 +15,9 @@ export class ProductService {
     private _http: Http
   ) { }
 
-  getAlbum(id: number) {
+  getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl).map(
-      response => response.json()
+      response => <Album>response.json()
     );
   }
 }
